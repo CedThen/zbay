f193e1f03c45f653a48facc5cb0fbb36a873e366
Ted <changxin.t.chen@gmail.com>
2022-02-20 16:08:17 -0500

    cart hooked up, needs styling

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,10 +1,55 @@
-
+import { useSelector, useDispatch } from "react-redux"
+import useCustomNavigate from "../hooks/useCustomNavigate"
+import { routeNames } from "./constants"
 
 const Cart = () => {
-  return (
+  const { isLoggedIn, products, user: { cart } } = useSelector(state => state)
+  const navLogin = useCustomNavigate(routeNames.LOGIN)
+  const navRegister = useCustomNavigate(routeNames.REGISTER)
+  console.log('products, cart', products, cart)
+  function LoginWarning() {
+    return (
+      <div className="m-5 p-5 border">Please
+        <span className="text-blue-400 cursor-pointer" onClick={navLogin}> Login </span>
+        or
+        <span className="text-blue-400 cursor-pointer" onClick={navRegister}> Register </span>
+        to view your cart</div>
+    )
+  }
 
-    <div>this is the cart</div>
+  function CartRows() {
+    return Object.keys(cart).map(itemId => {
+      const { image, title, price } = products.find(product => product.id === parseInt(itemId))
+      return (
+        <div className="w-full h-48 flex flex-row border">
+          <img src={image} alt='' />
+          <div>{title}</div>
+          <div>Quantity: {cart[itemId]}</div>
+          <div>{price}</div>
+        </div>
+      )
+    })
+  }
+
+  return (
+    <div className="w-full flex flex-col border-box mt-16">
+      <h1 className="text-4xl mb-5">Cart</h1>
+      {
+        !isLoggedIn ? <LoginWarning />
+          : (
+            <>
+              <div >
+                <CartRows />
+              </div>
+              <div>
+                <button>Check Out</button>
+              </div>
+            </>
+          )
+      }
+    </div >
   )
 }
+
 
 export default Cart
--- client/src/hooks/useCustomNavigate.jsx
+++ client/src/hooks/useCustomNavigate.jsx
@@ -1,7 +1,10 @@
 import { useNavigate } from "react-router-dom";
-
+import { routeNames } from "../components/constants";
 const useCustomNavigate = (path, state) => {
   const navigate = useNavigate();
+  // const paths = {
+  //   [routeNames.Login]: navigate(routeNames.LOGIN, state)
+  // }
   return () => navigate(path, state)
 }
 
